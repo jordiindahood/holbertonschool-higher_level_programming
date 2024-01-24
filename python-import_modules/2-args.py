5e509bcb36c8c58a575a4c6d6db6df043b83d6fb
jordiindahood <hazem.ouni123@gmail.com>
2024-01-25 00:43:45 +0100

    script 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    if len(argv) == 1:
+        print("{} arguments.".format(len(argv)-1))
+    else:
+        print("{} arguments:".format(len(argv)-1))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
