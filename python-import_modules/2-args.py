8e05c77eb6e05109415b352e2372e4d91f6f4cee
jordiindahood <hazem.ouni123@gmail.com>
2024-01-25 00:46:33 +0100

    script 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     argv = sys.argv
     if len(argv) == 1:
         print("{} arguments.".format(len(argv)-1))
+    elif len(argv) == 2:
+        print("{} argument.".format(len(argv)-1))
     else:
         print("{} arguments:".format(len(argv)-1))
     for i in range(1, len(argv)):
