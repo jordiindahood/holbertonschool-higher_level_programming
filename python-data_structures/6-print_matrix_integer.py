823de970a57a5ae89a08f0c12e2db2869f4e20f6
jordiindahood <hazem.ouni123@gmail.com>
2024-01-26 12:10:23 +0100

    script 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
     for vec in range(len(matrix)):
         for idx in range(len(matrix[vec])):
             print("{:d}".format(matrix[vec][idx]), end="")
