1bc8cbdad39596870d389196919854166f8298fd
jordiindahood <hazem.ouni123@gmail.com>
2024-01-26 12:08:37 +0100

    script 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for vec in range(len(matrix)):
         for idx in range(len(matrix[vec])):
             print("{}".format(matrix[vec][idx]), end="")
-            if idx < 3:
+            if idx < 2:
                 print(end=" ")
         print()
