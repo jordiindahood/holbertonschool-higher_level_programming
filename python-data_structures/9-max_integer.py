0a4ea6d99922555504af24dbae9fe49a98da41eb
jordiindahood <hazem.ouni123@gmail.com>
2024-01-26 14:42:49 +0100

    script 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list != None:
+    maxi = None
+    if my_list is not None:
         maxi = my_list[0]
         for idx in my_list:
             if maxi < idx:
                 maxi = idx
-        return maxi
-    return None
+    return maxi
