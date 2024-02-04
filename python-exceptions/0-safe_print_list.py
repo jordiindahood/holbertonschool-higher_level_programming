848fc2cd6be2179575632d48af7e21a7d31c0917
jordiindahood <hazem.ouni123@gmail.com>
2024-02-04 16:52:55 +0100

    scrrrript 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,6 +5,8 @@ def safe_print_list(my_list=[], x=0):
         for idx in range(x):
             print("{}".format(my_list[idx]), end="")
             case_num += 1
-    except:
+    except IndexError:
+        pass
+    finally:
         print()
     return case_num
