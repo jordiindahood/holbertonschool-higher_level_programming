ae22cb74a7110afe377e7426800019ebef410d26
jordiindahood <hazem.ouni123@gmail.com>
2024-02-04 17:49:55 +0100

    scrrrript 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except (ValueError, TypeError, ZeroDivisionError):
         x = None
     finally:
-        print(f"Inside result: {x}")
+        print("Inside result: {}".format(x))
     return x
