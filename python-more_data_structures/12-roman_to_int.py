331c3bb34d4fec290f87a73c90d0b4979999988c
jordiindahood <hazem.ouni123@gmail.com>
2024-02-04 16:09:52 +0100

    script 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    _ro = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    num = 0
+    prev = 0
+
+    if roman_string is None:
+        return 0
+    for ky in roman_string[::-1]:
+        val = _ro[ky]
+        if val < prev:
+            num -= val
+        else:
+            num += val
+        prev = val
+    return num
