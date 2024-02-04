290663ef6870793a05e0295c6704498e4bd930c1
jordiindahood <hazem.ouni123@gmail.com>
2024-02-04 15:45:56 +0100

    script 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,6 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     best = max(a_dictionary.values())
+
     for ky, vl in a_dictionary.items():
         if vl == best:
-            return str(ky)
+            return ky
