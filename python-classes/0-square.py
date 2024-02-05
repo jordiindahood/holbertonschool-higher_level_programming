f749082dcf216a60949bfffb4a5a27a53d5a3fc2
jordiindahood <hazem.ouni123@gmail.com>
2024-02-05 17:54:29 +0100

    skrpt 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+""" the file for the Square class definition
+"""
+
 class Square:
+    """a class that creates an empty square
+
+    Keyword arguments:
+    argument -- none
+    Return: none
+    """
+
     pass
