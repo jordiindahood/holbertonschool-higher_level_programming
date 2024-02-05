ca1861547e79e334dfd9ddf981e5d637effd1f25
jordiindahood <hazem.ouni123@gmail.com>
2024-02-05 18:56:56 +0100

    skrpt 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" the file for the Square class definition
+"""
+
+
+class Square:
+    """a class that creates a square
+
+    Keyword arguments:
+    argument -- none
+    Return: none
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise Exception("size must be an integer")
+        if size < 0:
+            raise Exception("size must be >= 0")
+        self._Square__size = size
+        pass
