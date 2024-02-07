c437e50655b18433c3bb69c3e12d7df5739fdf00
jordiindahood <hazem.ouni123@gmail.com>
2024-02-07 11:36:29 +0100

    skript 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""sumary_line
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
+
+
+def add_integer(a, b=98):
+    """ Keyword arguments:
+    argument -- a, b
+    Return: int """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
