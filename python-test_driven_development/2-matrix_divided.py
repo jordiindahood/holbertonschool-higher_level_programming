6925a707bb554ca5ee95e72c97bfe500046b1220
jordiindahood <hazem.ouni123@gmail.com>
2024-02-08 17:43:55 +0100

    script 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
 """ holberton-school task 1 module file
-Keyword arguments:
-argument -- description
-Return: return_description
 """
 
 
 def matrix_divided(matrix, div):
-    """Keyword arguments:
-    argument -- [[]] , int
-    Return: lis [[]]"""
+    """
+    Divides all elements of a matrix by the specified divisor.
+
+    Parameters:
+    - matrix (list of lists): The matrix to be divided.
+    - div (int or float): The divisor.
+
+    Returns:
+    - new_matrix (list of lists): The resulting matrix after division.
+    """
+
     for lis in matrix:
         if len(matrix[0]) != len(lis):
             raise TypeError("Each row of the matrix must have the same size")
         for elem in lis:
             if not type(elem) in [int, float]:
                 raise TypeError(
-                    "matrix must be a matrix " +
-                    "(list of lists) of integers/floats"
+                    "matrix must be a matrix " + "(list of lists) of integers/floats"
                 )
     if not type(div) in [int, float]:
-        raise TypeError(
-            "matrix must be a matrix " +
-            "(list of lists) of integers/floats"
-        )
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
