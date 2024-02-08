b55a4178a1ed45b99a1af465ca0d67d232f04bea
jordiindahood <hazem.ouni123@gmail.com>
2024-02-08 18:39:56 +0100

    script 3

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" holberton-school task 3 module file
+"""
+
+
+def print_square(size):
+    """
+    draw a square with '#' depending on size
+    Parameters:
+    - size: int
+    """
+    if type(size) not in [int, float]:
+        raise TypeError("size must be an integer")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise TypeError("size must be >= 0")
+
+    for line in range(size):
+        print("#" * size)
