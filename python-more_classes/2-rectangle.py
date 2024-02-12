fdabf4dff438f792419c41e06dc7e0187a7a5702
jordiindahood <hazem.ouni123@gmail.com>
2024-02-12 11:54:11 +0100

    script 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+"""
+This script defines a Rectangle class with
+properties for width and height,
+which are enforced to be positive integers.
+"""
+
+
+class Rectangle:
+
+    """
+    Constructor for the Rectangle class,
+    initializing a rectangle with the
+    given width and height values.
+    Defaults to zero if no values are provided.
+    """
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    """
+    Property getter for the height attribute,
+    returns the private variable height.
+    """
+
+    @property
+    def height(self):
+        return self.__height__
+
+    """
+    Property setter for the height attribute,
+    validating that the input is an
+    integer and greater than or equal to 0. Raises a TypeError otherwise.
+    """
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height__ = value
+
+    """
+    Property getter for the width attribute,
+    returns the private variable width.
+    """
+
+    @property
+    def width(self):
+        return self.__width__
+
+    """
+    Property setter for the width attribute,
+    validating that the input is an
+    integer and greater than or equal to 0. Raises a TypeError otherwise.
+    """
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width__ = value
+
+    def area(self):
+        return self.__height__ * self.__width__
+
+    def perimeter(self):
+        return self.__height__ * 2 + self.__width__ * 2
