f24ff84f9b78c06678304d77c4b59e1a554ca848
jordiindahood <hazem.ouni123@gmail.com>
2024-02-12 11:26:37 +0100

    script1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -26,10 +26,10 @@ class Rectangle:
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__rectangle__height = value
+
+        self.__rectangle__height = value
 
     """Proprety: getter to the width attribute"""
 
@@ -43,7 +43,7 @@ class Rectangle:
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__rectangle__width = value
+
+        self.__rectangle__width = value
