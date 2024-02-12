b9711a7d8f637fe447aae0e9b98c6ff3649aaa37
jordiindahood <hazem.ouni123@gmail.com>
2024-02-12 21:14:09 +0100

    script 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,5 +5,9 @@ task 2 file
 
 
 def is_same_class(obj, a_class):
-    """return true if type(obj) is a_class"""
-    return type(obj) == a_class
+    """
+    obj: obj
+    a_class: class
+    return true if type(obj) is a_class
+    """
+    return type(obj) is a_class
