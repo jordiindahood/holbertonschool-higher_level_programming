4a9dadc35d533f7b2c9d2b33935ba2632b0685c7
jordiindahood <hazem.ouni123@gmail.com>
2024-02-12 15:13:40 +0100

    skriipt 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,4 +3,9 @@
     Inheritance: module file
 """
 def lookup(obj):
+    """
+        lookup: a function that returns a list
+        containing available attributes and methods 
+        of an object {obj}
+    """
     return dir(obj)
