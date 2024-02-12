e6188f2053bdc5a327ba1fcdd8b8740d35f9af0b
jordiindahood <hazem.ouni123@gmail.com>
2024-02-12 13:37:03 +0100

    script 9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -81,8 +81,8 @@ class Rectangle:
 
     def __str__(self):
         if self.rec_height != 0 and self.rec_width != 0:
-            sy = self.print_symbol
-            strr = (str(sy) * self.rec_width + "\n") * self.rec_height
+            strr = (str(self.print_symbol)
+                    * self.rec_width + "\n") * self.rec_height
         else:
             return ""
         return strr[:-1]
@@ -109,9 +109,10 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """
-        make a square form rectangle class
-        width == height == size."
-        Returns a new Rectangle instance with:
+        """square form Rectangle class
+
+        Keyword arguments:
+        size: int
+        Return: square (object of Rectangle)
         """
         return cls(size, size)
