9014fd6f92e26cd5359f2f803c27b224a04f0391
jordiindahood <hazem.ouni123@gmail.com>
2024-02-12 20:57:51 +0100

    im totally done

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,21 +2,20 @@
 """
     A subclass of list that provides a method to
     print a sorted version of itself.
-"""
+    """
 
 
 class MyList(list):
-    """a subclass of List
+    """
+    A subclass of list that provides a method to
+    print a sorted version of itself.
 
-    Keyword arguments:
-    argument -- description
-    Return: None
+    Attributes:
+        new_list (list): An instance of a list.
     """
 
     def print_sorted(self):
         """
-        ########################################
-        #Prints the sorted version of the list.#
-        ########################################
+        Prints the sorted version of the list.
         """
         print(sorted(self))
