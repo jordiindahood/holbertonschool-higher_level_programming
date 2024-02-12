6959c4f368cc9dfd604786b39fb01d424bd9ffe8
jordiindahood <hazem.ouni123@gmail.com>
2024-02-12 21:21:02 +0100

    script 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+task 2 file
+2-is_same_class ??
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    obj: obj
+    a_class: class
+    return true if type(obj) is a_class
+    """
+    return isinstance(obj, a_class)
