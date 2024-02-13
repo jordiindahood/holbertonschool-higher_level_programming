2451164319185936e84016a42bd442a72617d509
jordiindahood <hazem.ouni123@gmail.com>
2024-02-13 16:31:51 +0100

    script 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+    file input-output: task 0 
+"""
 def read_file(file_name=""):
+    """a function that reads a file
+
+    Keyword arguments:
+    file_name -- name of file (str), "" if nothing
+    Return: none
+    """
+    
     with open(file_name) as f:
         print(f.read())
     f.closed
