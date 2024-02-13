f9f9f7b1f30f25eff15a72132052758991bab0e7
jordiindahood <hazem.ouni123@gmail.com>
2024-02-13 11:47:09 +0100

    script 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+task 5 file
+base_geometry class ??
+"""
+
+
+class BaseGeometry:
+    """
+    a class
+    Attributes: not yet
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
