df3d8d5bb1fc634c5d27da6f4ebf84318ba11427
jordiindahood <hazem.ouni123@gmail.com>
2024-02-13 13:48:57 +0100

    script 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+task 10 file
+Square class ??
+"""
+
+rec = __import__("9-rectangle").Rectangle
+
+
+class Square(rec):
+    """
+    a class based on BaseGeometry class
+    Attributes: not yet
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
