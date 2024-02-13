76ac603b1239d51304bdee897376ba4ad6a9ffda
jordiindahood <hazem.ouni123@gmail.com>
2024-02-13 11:57:14 +0100

    script 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+task 5 file
+base_geometry class ??
+"""
+
+
+class BaseGeometry:
+    """
+    a class
+    Attributes: not yet
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value == 0:
+            raise ValueError(f"{name} must be greater than 0")
