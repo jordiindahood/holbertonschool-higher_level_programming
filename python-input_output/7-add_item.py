50ab3746aac2c9722d02a3a47f5885cdcde5424a
jordiindahood <hazem.ouni123@gmail.com>
2024-02-15 00:27:56 +0100

    skrippt 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    file input-output:
+    task 7
+"""
+from sys import argv
+import os.path
+import json
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file()
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file()
+
+file = "add_item.json"
+
+if os.path.isfile(file):
+    data = load_from_json_file(file)
+else:
+    data = []
+save_to_json_file(list(data + argv[1:]),file)
