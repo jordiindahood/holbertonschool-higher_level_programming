727f2ebbda55c335f1a653e26de142ca0f6cfc30
jordiindahood <hazem.ouni123@gmail.com>
2024-02-14 23:50:39 +0100

    script6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    file input-output:
+    task 6
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """function that creates an Object from a JSON
+    Keyword arguments:
+    my_str -- str
+    Return: none
+    """
+    with open(filename, "r") as f:
+        json.load(f)
