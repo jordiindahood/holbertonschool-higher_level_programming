23a4ca42d83725d8409cd79ae5b5263b29f6e51b
jordiindahood <hazem.ouni123@gmail.com>
2024-02-15 12:09:36 +0100

    skrippt 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+    file input-output:
+    task 11
+"""
+
+
+class Student:
+    """
+    a that defines a student
+    public instance attrihbtes:
+    first_name -- str
+    last_name -- str
+    age -- int
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieve a dictionary representation of student"""
+        dikt = dict()
+        if attrs is None:
+            return self.__dict__
+        else:
+            for idx in attrs:
+                if idx in self.__dict__.keys():
+                    dikt.update(dict(idx=self.__dict__[idx]))
+            return dikt
+
+    def reload_from_json(self, json):
+        for ky, vl in json.items():
+            if hasattr(self, ky):
+                setattr(self, ky, vl)
