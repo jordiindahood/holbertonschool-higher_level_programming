0390d43c310730c6f1dff172a608d211a071aaff
jordiindahood <hazem.ouni123@gmail.com>
2024-02-16 23:50:49 +0100

    adding to_dictionary func to Square class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -20,7 +20,8 @@ if __name__ != "__main__":
 
         def __str__(self):
             """string representation of Square class"""
-            S_str = str(f"[Square] ({self.id}) {self.x}/{self.y}" + f" - {self.width}")
+            S_str = str(f"[Square] ({self.id}) {self.x}/{self.y}" 
+                        + f" - {self.width}")
             return S_str
 
         @property
