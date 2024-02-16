4107131f78f9565ade8e4b6cba7625b7b390937c
jordiindahood <hazem.ouni123@gmail.com>
2024-02-16 13:40:20 +0100

    task 4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,26 +17,27 @@ if __name__ != "__main__":
 
         def __init__(self, width, height, x=0, y=0, id=None):
             """Class init"""
-            if not isinstance(width, int):
-                raise TypeError("width must be an integer")
-            if width <= 0:
-                raise ValueError("width must be > 0")
+            if True:
+                if not isinstance(width, int):
+                    raise TypeError("width must be an integer")
+                if width <= 0:
+                    raise ValueError("width must be > 0")
 
-            if not isinstance(height, int):
-                raise TypeError("height must be an integer")
-            if height <= 0:
-                raise ValueError("height must be > 0")
+                if not isinstance(height, int):
+                    raise TypeError("height must be an integer")
+                if height <= 0:
+                    raise ValueError("height must be > 0")
 
-            if not isinstance(x, int):
-                raise TypeError("x must be an integer")
-            if x < 0:
-                raise ValueError("x must be >= 0")
+                if not isinstance(x, int):
+                    raise TypeError("x must be an integer")
+                if x < 0:
+                    raise ValueError("x must be >= 0")
 
-            if not isinstance(y, int):
-                raise TypeError("y must be an integer")
-            if y < 0:
-                raise ValueError("y must be >= 0")
-
+                if not isinstance(y, int):
+                    raise TypeError("y must be an integer")
+                if y < 0:
+                    raise ValueError("y must be >= 0")
+            # initialization
             super().__init__(id)
             self.__width = width
             self.__height = height
@@ -94,3 +95,10 @@ if __name__ != "__main__":
             if value < 0:
                 raise ValueError("y must be >= 0")
             self.__y = value
+
+        def area(self):
+            """
+            area():
+            this function returns the area of a rectangle
+            """
+            return self.__height * self.__width
