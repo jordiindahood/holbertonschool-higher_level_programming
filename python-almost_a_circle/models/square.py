bf63152c1c53f7ab52306538ed686f27509691aa
jordiindahood <hazem.ouni123@gmail.com>
2024-02-16 22:07:17 +0100

    j'aime tous ce qui est carré./main.py

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!/urs/bin/python3
+"""
+    Square class file
+"""
+from .rectangle import Rectangle
+
+if __name__ != "__main__":
+
+    class Square(Rectangle):
+        """
+        Rectangle: a class that inherited from Base class
+        width: int
+        height: int
+        x,y: int
+        id: int , None if not assigned
+        """
+
+        def __init__(self, size, x=0, y=0, id=None):
+            """Class init"""
+            super().__init__(size, size, x, y, id)
+
+        def __str__(self):
+            S_str = str(
+                f"[Square] ({self.id}) {self.x}/{self.y}" + 
+                f" - {self.width}"
+            )
+            return S_str
