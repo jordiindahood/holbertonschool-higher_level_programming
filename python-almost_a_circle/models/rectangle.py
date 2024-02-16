f8ea01daaaeb53949c359d9ec9490eddb08d7751
jordiindahood <hazem.ouni123@gmail.com>
2024-02-16 15:00:38 +0100

    task 8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -119,3 +119,15 @@ if __name__ != "__main__":
                 (self.__x * " " + "#" * self.__width + "\n") * self.__height
             )
             print(the_form[:-1])
+
+        def update(self, *args):
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
