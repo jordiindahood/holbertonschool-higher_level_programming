701cedd18ba55913d0fb983a71958e7f34e5b4aa
jordiindahood <hazem.ouni123@gmail.com>
2024-02-16 12:32:51 +0100

    correction of check 7 task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,43 +17,43 @@ if __name__ != "__main__":
 
         def __init__(self, width, height, x=0, y=0, id=None):
             super().__init__(id)
-            self.__width = width
-            self.__height = height
-            self.__x = x
-            self.__y = y
+            self.width = width
+            self.height = height
+            self.x = x
+            self.y = y
 
         # getter/setter : width
         @property
-        def width(self):
-            return self.__width
+        def fwidth(self):
+            return self.width
 
-        @width.setter
-        def width(self, value):
-            self.__width = value
+        @fwidth.setter
+        def fwidth(self, value):
+            self.width = value
 
         # getter/setter : height
         @property
-        def height(self):
-            return self.__height
+        def fheight(self):
+            return self.height
 
-        @height.setter
-        def height(self, value):
-            self.__height = value
+        @fheight.setter
+        def fheight(self, value):
+            self.height = value
 
         # getter/setter : x
         @property
         def x_axis(self):
-            return self.__x
+            return self.x
 
         @x_axis.setter
         def x_axis(self, value):
-            self.__x = value
+            self.x = value
 
         # getter/setter : y
         @property
         def y_axis(self):
-            return self.__y
+            return self.y
 
         @y_axis.setter
         def y_axis(self, value):
-            self.__y = value
+            self.y = value
