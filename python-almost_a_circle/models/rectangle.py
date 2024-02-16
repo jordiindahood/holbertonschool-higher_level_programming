4a288dbe0a6650574a2a0a6b6d57a6ce0aebb7eb
jordiindahood <hazem.ouni123@gmail.com>
2024-02-16 12:26:15 +0100

    correction of check 7 task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -16,12 +16,11 @@ if __name__ != "__main__":
         """
 
         def __init__(self, width, height, x=0, y=0, id=None):
+            super().__init__(id)
             self.__width = width
             self.__height = height
             self.__x = x
             self.__y = y
-            self.id = id
-            super().__init__(id)
 
         # getter/setter : width
         @property
