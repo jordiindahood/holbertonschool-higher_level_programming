8314b5118cf6d73a4aada1359e48f23cef1e30c9
jordiindahood <hazem.ouni123@gmail.com>
2024-02-16 12:00:19 +0100

    correction of check 7 task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,11 @@ from . import base
 
 class Rectangle(base.Base):
     """
-        Rectangle: a class that inherited from Base class
-        width: int
-        height: int
-        x,y: int
-        id: int , None if not assigned
+    Rectangle: a class that inherited from Base class
+    width: int
+    height: int
+    x,y: int
+    id: int , None if not assigned
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
