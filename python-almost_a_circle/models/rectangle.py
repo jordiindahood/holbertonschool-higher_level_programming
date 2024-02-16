d1766b001f3528a13dad373154b86a1af99bd65e
jordiindahood <hazem.ouni123@gmail.com>
2024-02-16 12:15:03 +0100

    correction of check 7 task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,57 +4,57 @@
 """
 from .base import Base
 
+if __name__ != "__main__":
 
+    class Rectangle(Base):
+        """
+        Rectangle: a class that inherited from Base class
+        width: int
+        height: int
+        x,y: int
+        id: int , None if not assigned
+        """
 
-class Rectangle(Base):
-    """
-    Rectangle: a class that inherited from Base class
-    width: int
-    height: int
-    x,y: int
-    id: int , None if not assigned
-    """
+        def __init__(self, width, height, x=0, y=0, id=None):
+            self.__width = width
+            self.__height = height
+            self.__x = x
+            self.__y = y
+            self.id = id
+            super().__init__(id)
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-        self.id = id
-        super().__init__(id)
+        # getter/setter : width
+        @property
+        def width(self):
+            return self.__width
 
-    # getter/setter : width
-    @property
-    def width(self):
-        return self.__width
+        @width.setter
+        def width(self, value):
+            self.__width = value
 
-    @width.setter
-    def width(self, value):
-        self.__width = value
+        # getter/setter : height
+        @property
+        def height(self):
+            return self.__height
 
-    # getter/setter : height
-    @property
-    def height(self):
-        return self.__height
+        @height.setter
+        def height(self, value):
+            self.__height = value
 
-    @height.setter
-    def height(self, value):
-        self.__height = value
+        # getter/setter : x
+        @property
+        def x_axis(self):
+            return self.__x
 
-    # getter/setter : x
-    @property
-    def x_axis(self):
-        return self.__x
+        @x_axis.setter
+        def x_axis(self, value):
+            self.__x = value
 
-    @x_axis.setter
-    def x_axis(self, value):
-        self.__x = value
+        # getter/setter : y
+        @property
+        def y_axis(self):
+            return self.__y
 
-    # getter/setter : y
-    @property
-    def y_axis(self):
-        return self.__y
-
-    @y_axis.setter
-    def y_axis(self, value):
-        self.__y = value
+        @y_axis.setter
+        def y_axis(self, value):
+            self.__y = value
