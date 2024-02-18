b9caf5d3a7363273f32503e638a382746857ad49
jordiindahood <hazem.ouni123@gmail.com>
2024-02-18 13:33:34 +0100

    script 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,7 +22,7 @@ if __name__ != "__main__":
             else:
                 Base.__nb_objects += 1
                 self.id = self.__nb_objects
-
+        @staticmethod
         def to_json_string(list_dictionaries):
             if list_dictionaries is None:
                 return "[]"
