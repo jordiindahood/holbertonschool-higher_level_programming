b3f3f804b97b95a732ffed456a1c8c23e9a58d72
jordiindahood <hazem.ouni123@gmail.com>
2024-03-24 23:48:09 +0100

    script7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This script lists all State objects
+from the database `hbtn_0e_6_usa`.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+def main(username, password, db_name):
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    # Create the database connection
+    cnt = f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}"
+    # Create the engine
+    engine = create_engine(cnt)
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # Create a Session
+    session = Session()
+    # Query the states and print it
+    for instance in session.query(State).order_by(State.id):
+        print(f"{instance.id}: {instance.name}")
+    # Close the session
+    session.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    main(username, password, db_name)
