8069a3c35225af4ea6d56cc77fdb601b6c54088f
jordiindahood <hazem.ouni123@gmail.com>
2024-03-24 22:44:36 +0100

    script7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- script that creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS `cities` (
+	`id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`state_id` int NOT NULL,
+	`name` varchar(256) NOT NULL,
+	FOREIGN KEY(`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`)
+);
